a3e69a54d36dbe04126373fe133dd2802ed8e3e6
romanlu <romanlu@playtika.com>
2020-10-21 12:14:16 +0300

    Updates

--- BomberMan/Assets/Scripts/Bomb.cs
+++ BomberMan/Assets/Scripts/Bomb.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bomb : MonoBehaviour
 {
@@ -19,6 +20,10 @@ public class Bomb : MonoBehaviour
         var colliders = Physics2D.OverlapCircleAll(transform.position, explosionRadius, explosionMask);
         foreach (var cldr in colliders)
         {
+            if (cldr.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
             Destroy(cldr.gameObject, explosionDelay);
         }
         Destroy(this.gameObject, explosionDelay);
---
+++ BomberMan/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var sequence = DOTween.Sequence();
+        sequence.Append(transform.DOMove(Vector3.left, 0.3f));
+        sequence.AppendInterval(0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+        
+}
