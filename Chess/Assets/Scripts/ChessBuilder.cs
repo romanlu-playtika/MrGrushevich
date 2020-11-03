3a1992d6cec016f3d29f561d3ef5d7d1029a4077
romanlu <romanlu@playtika.com>
2020-11-03 18:25:44 +0300

    rotation

--- Chess/Assets/Scripts/ChessBuilder.cs
+++ Chess/Assets/Scripts/ChessBuilder.cs
@@ -8,17 +8,20 @@ public class ChessBuilder : MonoBehaviour
 
     private void Awake()
     {
-        BuildFigures("WhiteRoot", chessData.WhiteFigures, chessData.WhitePosition);
+        BuildFigures("WhiteRoot", chessData.WhiteFigures, chessData.WhitePosition, true);
         BuildFigures("BlackRoot", chessData.BlackFigures, chessData.BlackPosition);
     }
 
-    private void BuildFigures(string rootName, List<BaseFigure> baseFigures, ChessPositionsData positionsData)
+    private void BuildFigures(string rootName, List<BaseFigure> baseFigures, ChessPositionsData positionsData, bool oppositeRotation = false)
     {
         var root = new GameObject(rootName);
         for (int i = 0; i < baseFigures.Count; i++)
         {
             var figure = Instantiate(baseFigures[i], root.transform);
             figure.transform.position = positionsData.Positions[i];
+            figure.transform.rotation = oppositeRotation
+                ? Quaternion.Euler(0, 180, 0)
+                : Quaternion.identity; 
             if (figure)
             {
                 figure.Initialize();
