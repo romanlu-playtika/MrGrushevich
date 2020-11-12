749548c3a0d28ec6f2090c7056c257bbcc228c07
romanlu <romanlu@playtika.com>
2020-11-12 18:28:12 +0300

    some changes

--- CustomFolders/Assets/Scripts/CharacterSelect.cs
+++ CustomFolders/Assets/Scripts/CharacterSelect.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 
 public class CharacterSelect : MonoBehaviour
 {
-    [SerializeField] private Button maleButton;
-    [SerializeField] private Button femaleButton;
     [SerializeField] private Transform playerRoot;
     private GameObject player;
+    private List <Texture2D> maleHairTextures = new List<Texture2D>();
+    private List <Texture2D> femaleHairTextures = new List<Texture2D>();
+    private List <Texture2D> maleClothesTextures = new List<Texture2D>();
+    private List <Texture2D> femaleClothesTextures = new List<Texture2D>();
 
     public void AddCharacter(string sex)
     {
@@ -23,10 +25,46 @@ public class CharacterSelect : MonoBehaviour
         player.transform.rotation = Quaternion.Euler(rot);
     }
 
-    public void ChangeTexture(string item)
+    public void ChangeTexture()
     {
+        Debug.Log("Getting");
         var directoryInfo = new DirectoryInfo(Application.streamingAssetsPath);
-        var directoryPath = Path.Combine(directoryInfo.ToString(), "Images");
-        
+
+        var allFiles = directoryInfo.GetFiles("*.tga");
+        foreach (var fileInfo in allFiles)
+        {
+            Debug.Log($"File name: {fileInfo.Name}");
+
+            var bytes = File.ReadAllBytes(fileInfo.FullName);
+            var texture2d = new Texture2D(1, 1);
+            texture2d.LoadImage(bytes);
+            if (fileInfo.Name.Contains("Female"))
+            {
+                if (fileInfo.Name.Contains("Hair"))
+                {
+                    femaleHairTextures.Add(texture2d);
+                }
+                else if (fileInfo.Name.Contains("Clothes"))
+                {
+                    femaleClothesTextures.Add(texture2d);
+                }
+            }
+            if (fileInfo.Name.Contains("Male"))
+            {
+                if (fileInfo.Name.Contains("Hair"))
+                {
+                    maleHairTextures.Add(texture2d);
+                }
+                else if (fileInfo.Name.Contains("Clothes"))
+                {
+                    maleClothesTextures.Add(texture2d);
+                }
+            }
+        }
+
+        foreach (var item in maleHairTextures)
+        {
+            Debug.Log($"{item.name}");
+        }
     }
 }
