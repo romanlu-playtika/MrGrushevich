86727773e6ee95519ab91697e62c5c83211223ee
romanlu <romanlu@playtika.com>
2020-11-12 17:52:24 +0300

    CharacterAdded

---
+++ CustomFolders/Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour
+{
+    [SerializeField] private Button maleButton;
+    [SerializeField] private Button femaleButton;
+    [SerializeField] private Transform playerRoot;
+    private GameObject player;
+
+    public void AddCharacter(string sex)
+    {
+        var prefab = Resources.Load<GameObject>($"Prefabs/{sex}");
+        if (player != null)
+        {
+            Destroy(player);
+        }
+        player = Instantiate(prefab, playerRoot);
+        var rot = new Vector3(0, -180, 0);
+        player.transform.rotation = Quaternion.Euler(rot);
+    }
+
+    public void ChangeTexture(string item)
+    {
+        var directoryInfo = new DirectoryInfo(Application.streamingAssetsPath);
+        var directoryPath = Path.Combine(directoryInfo.ToString(), "Images");
+        
+    }
+}
